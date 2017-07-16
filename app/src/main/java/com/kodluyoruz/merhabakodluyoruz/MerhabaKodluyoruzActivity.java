9f2af371e84582ea8197542b92b71ef7dc1c568a
Emre İŞBARALI <emre.isbarali@gmail.com>
2017-07-16 11:51:29 +0300

    İlk commit++

--- app/src/main/java/com/kodluyoruz/merhabakodluyoruz/MerhabaKodluyoruzActivity.java
+++ app/src/main/java/com/kodluyoruz/merhabakodluyoruz/MerhabaKodluyoruzActivity.java
@@ -9,7 +9,5 @@ public class MerhabaKodluyoruzActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_merhaba_kodluyoruz);
-
-
     }
 }
